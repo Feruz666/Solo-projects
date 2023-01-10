04ffa56e7ca915c191d09ecbf42bda9afc38144a
Umberman <f.kayumov97@gmail.com>
2023-01-10 18:10:38 +0500

    removed form tag

--- vite-meme-generator/meme-generator/src/components/Meme.jsx
+++ vite-meme-generator/meme-generator/src/components/Meme.jsx
@@ -4,15 +4,13 @@ import React from "react"
 export default function Meme() {
     return (
         <div className="form">
-            <form action="">
-                <div className="form-text-wrapper">
-                    <input className="form-text" type="text" placeholder="Top text" />
-                    <input className="form-text" type="text" placeholder="Bottom text" />
-                </div>
-                <p>
-                    <input className="form-submit" type="submit" value="Get a new meme image 🖼" />
-                </p>
-            </form>
+            <div className="form-text-wrapper">
+                <input className="form-text" type="text" placeholder="Top text" />
+                <input className="form-text" type="text" placeholder="Bottom text" />
+            </div>
+            <p>
+                <button className="form-submit">Get a new meme image 🖼</button>
+            </p>
         </div>
     )
 }
