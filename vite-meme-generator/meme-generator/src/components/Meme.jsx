e7bb5ae52db74b0d4b897a6c1f69725703a26907
Umberman <f.kayumov97@gmail.com>
2023-01-11 20:52:26 +0500

    minor changes

--- vite-meme-generator/meme-generator/src/components/Meme.jsx
+++ vite-meme-generator/meme-generator/src/components/Meme.jsx
@@ -1,16 +1,34 @@
 import React from "react"
+import data from "../../memeData.js"
 
 
 export default function Meme() {
+    const [memeImage, setMemeImage] = React.useState()
+    function GetImage() {
+        const memes = data.data.memes
+        const randomNumber = Math.floor(Math.random() * memes.length)
+
+        setMemeImage(function(prevMemeImage) {
+            return (
+                prevMemeImage = memes[randomNumber].url
+            )
+        })
+
+    }
     return (
-        <div className="form">
-            <div className="form-text-wrapper">
-                <input className="form-text" type="text" placeholder="Top text" />
-                <input className="form-text" type="text" placeholder="Bottom text" />
+        <main>
+            <div className="form">
+                <div className="form-text-wrapper">
+                    <input className="form-text" type="text" placeholder="Top text" />
+                    <input className="form-text" type="text" placeholder="Bottom text" />
+                </div>
+                <p>
+                    <button className="form-button" onClick={GetImage}>
+                        Get a new meme image 🖼
+                    </button>
+                </p>
             </div>
-            <p>
-                <button className="form-submit">Get a new meme image 🖼</button>
-            </p>
-        </div>
+            <img src={memeImage} alt="meme" className="meme--image" />
+        </main>
     )
 }
